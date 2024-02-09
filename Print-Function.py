bda541969535780f029c57fd112db07c864b7548
sena-00 <156020094+sena-00@users.noreply.github.com>
2024-02-09 10:35:14 -0400

    Delete Print-Function.py

--- Print-Function.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input(""))
-    
-    # Ensure n is positive
-    if n > 0:
-        # Loop to print the pattern
-        for i in range(1, n + 1):
-            # Print the numbers without spaces or string methods
-            print(i, end="")
